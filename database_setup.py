9e1b8a2a0176309ea8c92736221d697c4720db6b
gettingalex <ar_creation@hotmail.com>
2023-10-23 18:38:06 -0300

    setup flask app, basic database and folders trees for css (static) and html template

---
+++ database_setup.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+# Connect to the database file
+conn = sqlite3.connect('bookings.db')
+
+# Create a table for user profiles
+conn.execute('''CREATE TABLE users
+             (user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+             name TEXT NOT NULL,
+             email TEXT NOT NULL,
+             phone TEXT NOT NULL,
+             password TEXT NOT NULL);''')
+
+# Create a table for the first product
+conn.execute('''CREATE TABLE product1
+             (product_id INTEGER PRIMARY KEY AUTOINCREMENT,
+             name TEXT NOT NULL,
+             description TEXT NOT NULL,
+             price REAL NOT NULL);''')
+
+# Create a table for the second product
+conn.execute('''CREATE TABLE product2
+             (product_id INTEGER PRIMARY KEY AUTOINCREMENT,
+             name TEXT NOT NULL,
+             description TEXT NOT NULL,
+             price REAL NOT NULL);''')
+
+# Create a table for bookings
+conn.execute('''CREATE TABLE bookings
+             (booking_id INTEGER PRIMARY KEY AUTOINCREMENT,
+             user_id INTEGER NOT NULL,
+             product_id INTEGER NOT NULL,
+             date TEXT NOT NULL,
+             time TEXT NOT NULL,
+             FOREIGN KEY (user_id) REFERENCES users(user_id),
+             FOREIGN KEY (product_id) REFERENCES product1(product_id),
+             FOREIGN KEY (product_id) REFERENCES product2(product_id));''')
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
---
+++ reservation.py
@@ -0,0 +1,14 @@
+import sqlite3
+from flask import Flask
+
+app = Flask(__name__)
+
+# Connect to the SQLite database
+conn = sqlite3.connect('bookings.db')
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
