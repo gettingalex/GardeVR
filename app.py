4733748630d4f7754c4c33a56940bc0c34012d26
gettingalex <ar_creation@hotmail.com>
2024-01-30 15:38:12 +0800

    Add stripe to app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, jsonify
+import os
+import stripe
 
 app = Flask(__name__)
 
 # Connect to the SQLite database
 conn = sqlite3.connect('bookings.db')
+
+stripe_keys = {
+    "secret_key": os.environ["STRIPE_SECRET_KEY"],
+    "publishable_key": os.environ["STRIPE_PUBLISHABLE_KEY"],
+}
+
+stripe.api_key = stripe_keys["secret_key"]
 
 @app.route('/')
 def hello_world():
