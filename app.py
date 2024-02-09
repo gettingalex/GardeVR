11c780c8c2943ae11971e87f9db91f48ebb0d29f
Alexandre <ar_creation@hotmail.com>
2024-02-09 18:04:55 +0800

    add price_id and id to db product table

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask, jsonify, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 import stripe
+from flask_migrate import Migrate
+import time
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,13 +14,16 @@ app = Flask(__name__)
 
 app.secret_key = '101010'  # replace with your actual secret key
 
+
+
+
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookings.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 domain_url = "http://127.0.0.1:5000/"
 
 stripe_keys = {
@@ -31,6 +36,7 @@ stripe.api_key = stripe_keys["secret_key"]
 #DB Model Setup
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
     firstname = db.Column(db.String(100), nullable=False)
     lastname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
@@ -41,10 +47,12 @@ class User(db.Model):
     
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
     product_name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(100), nullable=False) 
     price = db.Column(db.Integer, nullable=False)
     stock_quantity = db.Column(db.Integer, nullable=False)
+    price_id = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f'<Product {self.name}>'
@@ -70,11 +78,50 @@ def process_variable():
     # Now you can use 'price' in your application
     return 'Success!', 200
 
+# Can add recurring payment with  https://stripe.com/docs/recurring-payments#installment-plans
+@app.route("/create-installment-session")
+def create_installment_session():
+    stripe.api_key = stripe_keys["secret_key"]
+    price = session.get('price')  # retrieve price from session
+    try:
+        # Create new Checkout Session for the order
+        # Other optional params include:
+        # [billing_address_collection] - to display billing address details on the page
+        # [customer] - if you have an existing Stripe Customer ID
+        # [payment_intent_data] - lets capture the payment later
+        # [customer_email] - lets you prefill the email input in the form
+        # For full details see https:#stripe.com/docs/api/checkout/sessions/create
+
+        # ?session_id={CHECKOUT_SESSION_ID} means x6                                                                                                                                                                                                                                                                     the redirect will have the session ID set as a query param
+        checkout_session = stripe.SubscriptionSchedule.create(
+            customer='{{CUSTOMER_ID}}',
+            start_date="now",
+            end_behavior="cancel",
+            phases=[
+                {
+                "items": [
+                    {
+                    "price_data": {
+                        "currency": "usd",
+                        "product": "prod_PT4R8sMVH9bImH",
+                        "recurring": {"interval": "month"},
+                        "unit_amount": 774,
+                    },
+                    "quantity": 1,
+                    },
+                ],
+                "iterations": 6,
+                },
+            ],
+        )
+        return jsonify({"sessionId": checkout_session["id"]})
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
 @app.route("/create-checkout-session")
 def create_checkout_session():
     stripe.api_key = stripe_keys["secret_key"]
     price = session.get('price')  # retrieve price from session
-    stripe.api_key = stripe_keys["secret_key"]
     try:
         # Create new Checkout Session for the order
         # Other optional params include:
