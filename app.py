79028bd4a430ec59b41dae493fe64fa76214969d
Alexandre <ar_creation@hotmail.com>
2024-02-09 15:48:14 +0800

    Add checkout expire after 2 hours

--- app.py
+++ app.py
@@ -90,6 +90,7 @@ def create_checkout_session():
             cancel_url=domain_url + "cancelled",
             payment_method_types=["card"],
             mode="payment",
+            expires_at=int(time.time() + (3600 * 2)), # Configured to expire after 2 hours
             line_items=[
                 {
                     "price": price,
