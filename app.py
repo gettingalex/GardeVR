d9b90f6e251a306669312ea45797d7a8a1f59708
Alexandre <ar_creation@hotmail.com>
2024-04-01 14:28:00 +0700

    testing

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def db_dashboard():
         print(result)
         #data = [dict(row) for row in result]
         #tables_data[table] = data
-    return success
+    return 'testing'
     #return render_template('db_dashboard.html', tables=tables_data)
 
 
