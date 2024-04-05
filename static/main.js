f07c692941f4cf20e885460828cc2c6108205dfb
Alexandre <ar_creation@hotmail.com>
2024-04-05 12:50:15 +0700

    debugging main.js for termes.html to checkout

--- static/main.js
+++ static/main.js
@@ -38,13 +38,26 @@ fetch("/config")
       }
       window.location.href = '/termes?product_id=' + product_id + '&price_id=' + price_var + '&quantity=' + quantity;
   
+    }
+  )};
+});
+
+fetch("/config")
+.then((result) => { return result.json(); })
+.then((data) => {
+  // Initialize Stripe.js
+  const stripe = Stripe(data.publicKey);
+
   // Event handler: accept terms
   const acceptBtn = document.getElementById('accept-btn');
   acceptBtn.addEventListener("click", function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const product_id = urlParams.get('product_id');
-    const price_id = urlParams.get('price_id');
-    const quantity = urlParams.get('quantity');
+    //const urlParams = new URLSearchParams(window.location.search);
+    //const product_id = urlParams.get('product_id');
+    //const price_id = urlParams.get('price_id');
+    //const quantity = urlParams.get('quantity');
+    var price_var = this.getAttribute('price_ID'); // 'this' refers to the button that was clicked
+    var product_id = this.getAttribute('product_ID');
+    var quantity = this.getAttribute('data-quantity');
 
     fetch('/process_variable', {
       method: 'POST',
@@ -68,41 +81,9 @@ fetch("/config")
       console.log(res);
     });
   });
-    });
-  }
+});
 
 
-
-  
-  // Installment event handler
-  const buttonsTwo = document.getElementsByClassName("submitBtn-installment");
-  for(let i = 0; i < buttonsTwo.length; i++) {
-    buttonsTwo[i].addEventListener("click", function() {
-      var price_var = this.getAttribute('price_ID'); // 'this' refers to the button that was clicked
-      fetch('/process_variable', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({product_price: price_var}),
-      })
-      .then(response => response.text())
-      .then(() => {
-        // Get Checkout Session ID
-        return fetch("/create-installment-session")
-      })
-      .then((result) => { return result.json(); })
-      .then((data) => {
-        console.log(data);
-        // Redirect to Stripe Checkout
-        return stripe.redirectToCheckout({sessionId: data.sessionId})
-      })
-      .then((res) => {
-        console.log(res);
-      });
-    });
-  }
-}); // Add closing parenthesis and semicolon here
 
 
 
