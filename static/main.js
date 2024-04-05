f66dcdc5a49a773bf5aea17ad12b874a724af0bc
Alexandre <ar_creation@hotmail.com>
2024-04-05 13:50:59 +0700

    debug error line 53 in main.js Unhandled Promise Rejection: TypeError: null is not an object (evaluating 'acceptBtn.addEventListener')

--- static/main.js
+++ static/main.js
@@ -50,37 +50,39 @@ fetch("/config")
 
   // Event handler: accept terms
   const acceptBtn = document.getElementById('accept-btn');
-  acceptBtn.addEventListener("click", function() {
-    //const urlParams = new URLSearchParams(window.location.search);
-    //const product_id = urlParams.get('product_id');
-    //const price_id = urlParams.get('price_id');
-    //const quantity = urlParams.get('quantity');
-    var price_var = this.getAttribute('price_ID'); // 'this' refers to the button that was clicked
-    var product_id = this.getAttribute('product_ID');
-    var quantity = this.getAttribute('data-quantity');
+  if (acceptBtn) {
+    acceptBtn.addEventListener("click", function() {
+      //const urlParams = new URLSearchParams(window.location.search);
+      //const product_id = urlParams.get('product_id');
+      //const price_id = urlParams.get('price_id');
+      //const quantity = urlParams.get('quantity');
+      var price_var = this.getAttribute('price_ID'); // 'this' refers to the button that was clicked
+      var product_id = this.getAttribute('product_ID');
+      var quantity = this.getAttribute('data-quantity');
 
-    fetch('/process_variable', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({product_price: price_var, product_ID: product_id}),
-    })
-    .then(response => response.text())
-    .then(() => {
-      // Get Checkout Session ID
-      return fetch("/create-checkout-session")
-    })
-    .then((result) => { return result.json(); })
-    .then((data) => {
-      console.log(data);
-      // Redirect to Stripe Checkout
-      return stripe.redirectToCheckout({sessionId: data.sessionId})
-    })
-    .then((res) => {
-      console.log(res);
+      fetch('/process_variable', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({product_price: price_var, product_ID: product_id}),
+      })
+      .then(response => response.text())
+      .then(() => {
+        // Get Checkout Session ID
+        return fetch("/create-checkout-session")
+      })
+      .then((result) => { return result.json(); })
+      .then((data) => {
+        console.log(data);
+        // Redirect to Stripe Checkout
+        return stripe.redirectToCheckout({sessionId: data.sessionId})
+      })
+      .then((res) => {
+        console.log(res);
+      });
     });
-  });
+  };
 });
 
 
